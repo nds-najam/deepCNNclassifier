3ab3485c05f268f3be6819a1b25cdcbf4e876093
nds-najam <najam@nds-academy.com>
2022-11-07 00:38:16 +0530

    Stage-5 changes

---
+++ src/deepClassifier/pipeline/stage_04_evaluation.py
@@ -0,0 +1,23 @@
+from deepClassifier.config import ConfigurationManager
+from deepClassifier.components import Evaluation
+from deepClassifier import logger
+
+
+STAGE_NAME = "Evaluation Stage"
+
+def main():
+    config = ConfigurationManager()
+    val_config = config.get_validation_config()
+    evaluation = Evaluation(val_config)
+    evaluation.evaluation()
+    evaluation.save_score()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*************")
+        logger.info(f"\n\n>>>>>> stage {STAGE_NAME} started <<<<<<")
+        main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed \n\n<<<<<<")
+    except Exception as e:
+        logger.exception(e)
+        raise e
